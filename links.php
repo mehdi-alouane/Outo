258071c83701b13f428cc72dfe8de251745b2556
Medo alouane <pro4medo@gmail.com>
2014-12-16 14:43:01 +0000

    Update and rename index.php to links.php

--- links.php
+++ links.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * Important notes:
- * 
- * 1. The .htacess file should have the following lines:
- * 
- *    RewriteEngine On
- *    RewriteCond %{REQUEST_FILENAME} !-f
- *    RewriteCond %{REQUEST_FILENAME} !-d
- *    RewriteRule ^/?(.*) index.php [L]
- *
- * 2. All the pages should be saved as .php files
- *
- * 3. This URL Manager supposed that all the pages are stored under "pages" directory which
- *    is under the root directory (you can change it!).
- *
- * 4. The main layout (footer & header) should be stored under "layouts" directory which is
- *    under the root directory (you can change it!).
- */
 
 if (!empty($_SERVER['REQUEST_URI']))
 {
